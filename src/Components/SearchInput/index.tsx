3934daff7dd0b9e655904601154fdb3d5d011a73
Moises Menezes <moisesmenezes90@gmail.com>
2022-02-22 21:14:47 -0300

    chore: add debounce hook

--- src/Components/SearchInput/index.tsx
+++ src/Components/SearchInput/index.tsx
@@ -1,9 +1,16 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useDebounce from "../../hooks/useDebounce";
 
 const SearchInput = () => {
   const [search, setSearch] = useState("");
+
+  const debounceSearch = useDebounce(search, 1000);
+
+  useEffect(() => {
+    console.log("SEARCH", search);
+  }, [debounceSearch]);
 
   return (
     <InputGroup>
